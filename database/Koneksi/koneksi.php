9c373a25cd423b32329809b850eceb1268ce675e
daffadlr <daffadlrharis@gmail.com>
2023-12-05 21:08:28 +0700

    Perubahan ke3

--- database/Koneksi/koneksi.php
+++ database/Koneksi/koneksi.php
@@ -1,22 +1,19 @@
 <?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "cloth";
 
-namespace Database\Seeders;
+// Buat koneksi
+$conn = new mysqli($host, $user, $password, $database);
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+// Periksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
 }
+
+echo "Koneksi berhasil";
+
+// Jangan lupa untuk menutup koneksi setelah digunakan
+$conn->close();
+?>
